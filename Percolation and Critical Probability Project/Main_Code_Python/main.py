ce240f978f072e890079fbbd54c2061869a09841
SgaravitoWp <sgaravito@unal.edu.co>
2022-06-24 12:48:56 -0500

    CV

--- Percolation and Critical Probability Project/Main_Code_Python/main.py
+++ Percolation and Critical Probability Project/Main_Code_Python/main.py
@@ -1,25 +1,30 @@
+
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import colors
+import sys 
+
 
 class Net:
+
     def __init__(self, p, L, seed):
 
         self.p = p
         self.l = L
         self.seed = seed
-        self.__matrix = self.create_matrix()
+        self.__matrix = self.__create_matrix()
         self.__final_matrix = self.__matrix.copy()
         self.clustersNum = 2
+        self.__clusters_finding()
         
+    '''Metodos Privados'''
 
-    def set_seed(self):
+    def __set_seed(self):
         np.random.seed(self.seed)
 
-    def create_matrix(self):
+    def __create_matrix(self):
         
         if self.seed is not None:
-            self.set_seed()
+            self.__set_seed()
 
         start_matrix = np.ones(self.l+2, dtype=int)*-1
 
@@ -36,47 +41,77 @@ class Net:
         return random_matrix
     
 
-    def clusters_mark(self, i, j):
+    def __clusters_mark(self, i, j):
                    
             self.__final_matrix[i,j] = self.clustersNum
             if self.__final_matrix[i+1, j] == 1:
-                self.clusters_mark(i+1, j)
+                self.__clusters_mark(i+1, j)
             else:
                 pass
             if self.__final_matrix[i-1, j] == 1:
-                self.clusters_mark(i-1,j)
+                self.__clusters_mark(i-1,j)
             else:
                 pass
             if self.__final_matrix[i, j+1] == 1:
-                self.clusters_mark(i,j+1)
+                self.__clusters_mark(i,j+1)
             else:
                 pass
             if self.__final_matrix[i, j-1] == 1:
-                self.clusters_mark(i,j-1)
+                self.__clusters_mark(i,j-1)
             else:
                 pass
         
-    def clusters_finding(self):
+    def __clusters_finding(self):
         for i in range(self.l +2):
             for j in range(self.l +2):
                 if self.__final_matrix[i,j] != 1:
                     continue
                 else:
-                    self.clusters_mark(i, j)
+                    self.__clusters_mark(i, j)
                     self.clustersNum += 1         
 
-    def cut_matrix(self, type):
+    def __cut_matrix(self, type):
         if type == 'I':
             return self.__matrix[1:self.l+1,1:self.l+1]
         if type == 'F':
             return self.__final_matrix[1:self.l+1,1:self.l+1]
 
+    def __vertical_per_cluster(self):
+        
+        row_sup = set(self.__cut_matrix("F")[0])
+        row_inf = set(self.__cut_matrix("F")[self.l - 1])
+        global_cluster = list((row_sup & row_inf) - {0})
+        global_cluster = len(global_cluster)
+
+        return global_cluster
+    
+    def __horizontal_per_cluster(self):
+        
+        column_sup = set(np.transpose(self.__cut_matrix("F")[0:self.l,0]))
+        column_inf = set(np.transpose(self.__cut_matrix("F")[0:self.l,self.l - 1]))
+        global_cluster = list((column_sup & column_inf) - {0})
+        global_cluster = len(global_cluster)
+
+        return global_cluster
+
+    '''Metodos accesibles'''
+
+    def clusters_count(self):
+        return np.max(self.__final_matrix) -1
+
+    def per_cluster(self):
+        if self.__horizontal_per_cluster() + self.__vertical_per_cluster() == 0:
+            boolean = False
+        else:
+            boolean = True
+        return boolean
+
     def print_matrix(self, type, draw):
         if draw == "M":
-            print(self.cut_matrix(type))
+            print(self.__cut_matrix(type))
         if draw == "C":
             plt.figure(figsize=(5,5))
-            plt.pcolor(self.cut_matrix(type))
+            plt.pcolor(self.__cut_matrix(type))
             plt.axis('off')
             plt.show()
         
--- Percolation and Critical Probability Project/Main_Code_Python/methods.py
+++ Percolation and Critical Probability Project/Main_Code_Python/methods.py
@@ -1,17 +1,16 @@
-from random import seed
 from main import Net
 
 
 def main(p_, L_, seed_):
 
     net = Net(p = p_, L = L_, seed = seed_ )
-    net.clusters_finding()
-    net.print_matrix(type='F', draw="C")
+    net.print_matrix(type = "F", draw = "C")
+    print(net.per_cluster())
 
 if __name__ == "__main__":
-    p = ''
-    l = ''
-    seed = ''
+    p = 1
+    l = 10
+    seed = 5
     main(p, l, seed)
 
 
